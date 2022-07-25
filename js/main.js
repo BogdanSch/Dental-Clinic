c155096bc91b1ef345bf34d670107bd3fa629b6e
Bohdan Scherbak <bogmira777@gmail.com>
2022-07-25 22:50:39 +0200

    Added employers section

--- js/main.js
+++ js/main.js
@@ -8,24 +8,9 @@
             arrows: false,
             dots: true,
             speed: 2000,
-            autoplay: true,
+            autoplay: false,
             autoplaySpeed: 2000,
             infinite: true,
-            appendDots: '.method__dots',
-            // prevArrow: '.links-slider-prev',
-		    // nextArrow: '.links-slider-next',
-        });
-      });
-
-      $(document).ready(function(){
-        $('.method__slider').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: false,
-            dots: true,
-            speed: 2000,
-            autoplay: true,
-            autoplaySpeed: 2000,
             appendDots: '.method__dots',
         });
       });
